233b20f521a5c5deed1ccedf0aced7b78a876d10
heavensbee001 <heavensbee001@gmail.com>
2021-12-10 17:34:20 +0100

    Section 4 - Add random prizes when wave

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -8,6 +8,8 @@ contract WavePortal {
     uint256 totalWaves;
     uint256 totalHangLooses;
 
+    uint256 private seed;
+
     enum WaveType { WAVE, HANGLOOSE }
     struct Wave {
         WaveType _type;
@@ -22,10 +24,16 @@ contract WavePortal {
 
     constructor() payable {
         console.log("Yo yo, I am a contract and I am smart");
+
+        seed = (block.timestamp + block.difficulty) % 100;
     }
 
     function wave(WaveType _type, string memory _message) public {
         waves.push(Wave(_type, msg.sender, _message, block.timestamp));
+
+        seed = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %d", seed);
+
         if (_type == WaveType.WAVE) {
             totalWaves += 1;
             console.log("%s has waved!", msg.sender);
@@ -33,12 +41,16 @@ contract WavePortal {
             totalHangLooses += 1;
             console.log("%s has saluted with a shaka!", msg.sender);
         }
+
         emit NewWave(_type, msg.sender, block.timestamp, _message);
 
-        uint256 prizeAmount = 0.0001 ether;
-        require(prizeAmount <= address(this).balance,"Trying to withdraw more money than the contract has.");
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        uint256 prizeAmount = 0.001 ether;
+        if (seed <= 20) {
+            require(prizeAmount <= address(this).balance,"Trying to withdraw more money than the contract has.");
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        
     }
 
     function getAllWaves() public view returns (Wave[] memory) {
